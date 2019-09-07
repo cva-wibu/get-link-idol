0bce11059c7517a8e463f7349e48b43d56cc5bbd
long đoàn <brainzzz27@gmail.com>
2019-09-07 19:45:21 +0700

    Update code to create picture db

--- TODO.md
+++ TODO.md
@@ -2,6 +2,7 @@
 - [ ] merge small functions into a larger app
 - [ ] if cannot find link from code, try finding who's the idol and provide some other codes for the user to try
 - [x] crawl idol images, preferably 5-10 pics per idol
-- [ ] create idol databases
+- [ ] create idol database
+- [x] create idol picture database
 - [ ] perform facial recognition (?) to recognize idol
 - [ ] create demo web (using flask?)
--- crawler.py
+++ crawler.py
@@ -1,29 +1,70 @@
+import os
+from typing import Dict
+import sqlite3
+
 import requests
 import bs4 as bs
-import os
 
 rq = requests.Session()
 
+
+def create_db():
+    """
+    Create idol database. WARNING: will erase existing db
+    :return: connection, cur
+    """
+    conn = sqlite3.connect('idol.sqlite')
+    cur = conn.cursor()
+
+    try:
+        cur.execute('DROP TABLE picture')
+    except sqlite3.OperationalError:
+        pass
+
+    cur.execute('CREATE TABLE picture('
+                   'idol_name varchar(40),'
+                   'pic_name varchar(100))')
+
+    return conn, cur
+
+
 def search_idols():
-    SEARCH_URL  = "https://javmodel.com/jav/order_homepages.php?model_cat=6%20Stars%20JAV"
+    """
+    Create a list of all idols
+    :return: List of all idol names
+    """
+    SEARCH_URL = "https://javmodel.com/jav/order_homepages.php?model_cat=6%20Stars%20JAV"
 
-    page        = rq.get(SEARCH_URL)
-    soup        = bs.BeautifulSoup(page.text, 'lxml')
+    page = rq.get(SEARCH_URL)
+    soup = bs.BeautifulSoup(page.text, 'lxml')
 
-    results     = []
+    results = []
 
     # "text-center" class contains idol names and bio links
-    for search_result in soup.find_all(class_ = "text-center"):
-        idol    = search_result.select("a[href]")[0]
-        result  = { "profile_link"    :   f"https://javmodel.com{idol['href']}",
-                    "name"            :   idol.text}
+    for search_result in soup.find_all(class_="text-center"):
+        idol = search_result.select("a[href]")[0]
+        result = {"profile_link": f"https://javmodel.com{idol['href']}",
+                  "name": idol.text}
 
         results.append(result)
 
     return results
 
-def get_img(idol):
-    PATH = f'./data/{idol["name"]}'
+
+def get_img(idol: Dict):
+    """
+    Crawl idol images
+    :param idol: dictionary (name, profile_link) of a specific idol
+    :return: list of images of the idol
+    """
+    idol_name = idol['name']
+    # idol_name = '_'.join(idol_name.split())
+
+    PATH = os.getcwd()
+    PATH = os.path.join(PATH, 'data')
+    # PATH = os.path.join(PATH, idol_name)
+
+    # PATH = f'./data/{idol["name"]}'
     try:
         os.mkdir(PATH)
     except FileExistsError:
@@ -33,27 +74,32 @@ def get_img(idol):
 
     soup = bs.BeautifulSoup(page.text, 'lxml')
 
+    global img_count, conn, cur
     for image in soup.select("img[alt]"):
-        file_name = image['alt']
+        # file_name = f'{idol_name}_{image["alt"]}.jpg'
+        file_name = f'{image["alt"]}.jpg'
         image_url = image['src']
 
         response = rq.get(image_url)
-        if (response.status_code == 200):
-            global img_count
+        if response.status_code == 200:
             img_count += 1
-            with open(f'{PATH}/{file_name}.jpg', 'wb') as f:
+            with open(f'{os.path.join(PATH, file_name)}', 'wb') as f:
                 f.write(response.content)
+            cur.execute('INSERT INTO picture VALUES(?, ?)', (idol_name, file_name))
+            # print(cur.fetchone())
 
-if (__name__ == "__main__"):
+
+if __name__ == "__main__":
     try:
-        os.mkdir("./data")
+        os.mkdir('data')
     except FileExistsError:
         pass
 
-    idols       = search_idols()
+    conn, cur = create_db()
+    idols = search_idols()
 
-    idol_count  = 0
-    img_count   = 0
+    idol_count = 0
+    img_count = 0
 
     for idol in idols:
         idol_count += 1
@@ -61,3 +107,5 @@ if (__name__ == "__main__"):
         get_img(idol)
 
     print(f'Crawled {img_count} pictures from {idol_count} idols')
+    conn.commit()
+    conn.close()
