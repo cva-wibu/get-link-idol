1311c641ead95a38b43418505a4a94629a870941
Tam Nguyen <presariohg@gmail.com>
2019-09-09 01:39:57 +0200

    tried using aiohttp with positive results: reduced more than a half crawling time. Need to research more for further results

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ import os
 import requests
 import sqlite3
 import bs4 as bs
+import aiohttp
+import asyncio
 
 from typing import Dict
 
@@ -29,7 +31,10 @@ def create_db():
     return conn, cur
 
 
-def crawl_idols_data():
+img_count = 0
+
+
+async def crawl_idols_data():
     """
     Crawl every single idols, each ~10 images
     :return: a sqlite3 database of idol names and pictures
@@ -37,9 +42,8 @@ def crawl_idols_data():
     import re
 
     idol_count = 0
-    img_count = 0
 
-    first_page = "https://javmodel.com/jav/homepages.php?page=1"
+    first_page = "https://javmodel.com/jav/homepages.php?page=39"
     page = rq.get(first_page)
 
     total_idol_number = re.findall(r'Total (\d+?) JAVModels Found', page.text)[0]
@@ -47,45 +51,43 @@ def crawl_idols_data():
     next_page = first_page
 
     # repeat until reach last result page
-    while True:
-        page = rq.get(next_page)
-        soup = bs.BeautifulSoup(page.text, 'lxml')
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.get(next_page) as page:
+                soup = bs.BeautifulSoup(await page.text(), 'lxml')
 
-        # "text-center" class contains idol names and bio links
-        for search_result in soup.find_all(class_="text-center"):
-            result = search_result.select("a[href]")[0]
+                # "text-center" class contains idol names and bio links
+                for search_result in soup.find_all(class_="text-center"):
+                    result = search_result.select("a[href]")[0]
 
-            idol = {"profile_link": BASE_URL + result['href'],
-                    "name": result.text}
+                    idol = {"profile_link": BASE_URL + result['href'],
+                            "name": result.text}
 
-            idol_count += 1
-            print(f"Crawling {idol['name']} data ({idol_count}/{total_idol_number})")
-            get_img(idol)
+                    idol_count += 1
+                    print(f"Crawling {idol['name']} data ({idol_count}/{total_idol_number})")
+                    await get_img(idol)
 
-        try:
-            # find next page url
-            next_page = BASE_URL + (soup.find_all("a", string="Next")[0]['href'])
-        except IndexError:
-            # if not found then reached last page, break loop
-            break
+                try:
+                    # find next page url
+                    next_page = BASE_URL + (soup.find_all("a", string="Next")[0]['href'])
+                except IndexError:
+                    # if not found then reached last page, break loop
+                    break
 
     print(f'Crawled {img_count} pictures from {idol_count} idols')
 
 
-def get_img(idol: Dict):
+async def get_img(idol: Dict):
     """
     Crawl idol images
     :param idol: dictionary (name, profile_link) of a specific idol
     :return: list of images of the idol
     """
     idol_name = idol['name']
-    # idol_name = '_'.join(idol_name.split())
 
     PATH = os.getcwd()
     PATH = os.path.join(PATH, 'data')
-    # PATH = os.path.join(PATH, idol_name)
 
-    # PATH = f'./data/{idol["name"]}'
     try:
         os.mkdir(PATH)
     except FileExistsError:
@@ -95,19 +97,23 @@ def get_img(idol: Dict):
 
     soup = bs.BeautifulSoup(page.text, 'lxml')
 
+    session = aiohttp.ClientSession()
+
     global img_count, conn, cur
     for image in soup.select("img[alt]"):
-        # file_name = f'{idol_name}_{image["alt"]}.jpg'
         file_name = f'{image["alt"]}.jpg'
         image_url = image['src']
 
-        response = rq.get(image_url)
-        if response.status_code == 200:
-            img_count += 1
-            with open(f'{os.path.join(PATH, file_name)}', 'wb') as f:
-                f.write(response.content)
-            cur.execute('INSERT INTO picture VALUES(?, ?)', (idol_name, file_name))
-            # print(cur.fetchone())
+        async with session.get(image_url) as response:
+
+            if response.status == 200:
+                img_count += 1
+                with open(f'{os.path.join(PATH, file_name)}', 'wb') as f:
+                    f.write(await response.read())
+                cur.execute('INSERT INTO picture VALUES(?, ?)', (idol_name, file_name))
+                # print(cur.fetchone())
+
+    await session.close()
 
 
 if __name__ == "__main__":
@@ -118,7 +124,7 @@ if __name__ == "__main__":
 
     conn, cur = create_db()
 
-    crawl_idols_data()
+    asyncio.run(crawl_idols_data())
 
     conn.commit()
     conn.close()
